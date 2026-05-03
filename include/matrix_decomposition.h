8bc311a11b46854564d2947eaddb215578f275da
dochkavurdalaka <dmitriylapin2001@gmail.com>
2026-05-03 13:29:31 +0300

    add matrix_decomposition_update

--- include/matrix_decomposition.h
+++ include/matrix_decomposition.h
@@ -29,7 +29,6 @@ std::vector<double> GetHCoef(MKL_Complex16* hamiltonian, size_t N) {
         hamiltonian_diag_prefixsum[k] += hamiltonian_diag_prefixsum[k - 1];
     }
 
-
     for (size_t l = 0; l + 1 < N; ++l) {
         double coeff = hamiltonian_diag_prefixsum[l] / sqrt((l + 1) * (l + 2));
         size_t index = (l + 1) * N + (l + 1);
@@ -63,7 +62,6 @@ std::vector<MKL_Complex16> GetLCoef(MKL_Complex16* lindbladian, size_t N) {
             l_coeff.push_back(coeff / sqrt(2));
         }
     }
-
 
     std::vector<MKL_Complex16> lindbladian_diag_prefixsum(N - 1);
     for (size_t k = 0; k + 1 < N; ++k) {
@@ -103,7 +101,6 @@ double* GetVCoef(MKL_Complex16* rho, size_t N) {
         }
     }
 
-
     std::vector<double> rho_diag_prefixsum(N - 1);
     for (size_t k = 0; k + 1 < N; ++k) {
         rho_diag_prefixsum[k] = rho[k * N + k].real;
@@ -141,12 +138,19 @@ MKL_Complex16* GetDensityBySUDecomposition(double* v, size_t N) {
     }
 
     offset = N * (N - 1);
+    std::vector<double> diag_inverse_prefix_sum(N - 1);
+    for (size_t k = 0; k < N - 1; ++k) {
+        diag_inverse_prefix_sum[k] = v[offset + k] / sqrt((k + 1) * (k + 2));
+    }
 
-    for (size_t k = 0; k + 1 < N; ++k) {
-        rho_final[k * N + k] = {0, 0};
-        for (size_t l = k; l + 1 < N; ++l) {
-            rho_final[k * N + k].real += v[offset + l] / sqrt((l + 1) * (l + 2));
-        }
+    for (size_t k = 1; k < N - 1; ++k) {
+        size_t k_inv = N - 2 - k;
+        diag_inverse_prefix_sum[k_inv] += diag_inverse_prefix_sum[k_inv + 1];
+    }
+
+    for (size_t l = 0; l + 1 < N; ++l) {
+        rho_final[l * N + l] = {0, 0};
+        rho_final[l * N + l].real = diag_inverse_prefix_sum[l];
     }
 
     rho_final[N * N - 1] = {0., 0.};
